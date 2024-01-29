0c07e91835e15b04d22ac65d1fc01fdb5eefbc0c
Albert Pattarachote <albertpattarachote22@gmail.com>
2024-01-29 10:39:58 -1000

    promises

--- app.js
+++ app.js
@@ -15,8 +15,24 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+let errorP = document.querySelector("#error");
+let ul = document.querySelector("#list");
 
+let promise = getList();
+
+// TODO: Handle the resolved or rejected states of the promise
+function handleList(list){
+  list.forEach((hobbit) => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    ul.appendChild(li);
+  });
+}
+function handleErr(err){
+  errorP.textContent = err.message;
+  console.log(err);
+}
+promise.then(handleList).catch(handleErr);
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
 
